2561bb528c38ababa21447bab0e08195039550c6
Matthew M <mjmcgowan7@gmail.com>
2018-08-04 19:20:24 -0400

    Updated TODO list

--- Extension/background.js
+++ Extension/background.js
@@ -12,6 +12,11 @@ https://www.w3schools.com/js/js_timing.asp
 //Convert time to hours:minutes instead of seconds
 //Have time reset at a settable time and check against system clock (3am)
 //Have toggleable dev mode flag that will display init badge and console logs
+//Document/comment code so myself and others can comprehend it(content and background scripts)
+//store config in local storage somehow
+//store time so it persists through closing chrome
+//clean up css and get rid of old code
+//Make bg only query tabs if youtube is open
 
 //Timer code
 var time = 0;
