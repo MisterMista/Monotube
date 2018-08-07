fe76e992b1f8d5de61826eadb4560ce3276faa4d
Matthew M <mjmcgowan7@gmail.com>
2018-08-06 21:45:28 -0400

    Stores time in local storage Time will now persist through chrome window closing.

--- Extension/background.js
+++ Extension/background.js
@@ -8,9 +8,6 @@ https://www.w3schools.com/js/js_timing.asp
 //Make official icon for extension
 //Have time reset at a settable time and check against system clock (3am)
 //Have toggleable dev mode flag that will display init badge and console logs
-//Document/comment code so myself and others can comprehend it(content and background scripts)
-//store config in local storage somehow
-//store time so it persists through closing chrome
 
 console.log("background.js loaded");
 
@@ -24,6 +21,8 @@ var isYoutubeOpen = 0;
 //Initializes browser action badge
 chrome.browserAction.setBadgeText({text: "init"});
 chrome.browserAction.setBadgeBackgroundColor({color: [0, 127, 127, 255]});
+
+load();
 
 //adds leading zero to single digit number
 function pad(n){return(n<10?"0":"")+n};
@@ -57,6 +56,18 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
+function save() {
+  chrome.storage.local.set({"secStorage": sec}, function() {console.log("Saved")});
+}
+function load() {
+  chrome.storage.local.get("secStorage", function(result) {
+    if (result.secStorage !== undefined) {
+      chrome.browserAction.setBadgeBackgroundColor({color: [127, 127, 255, 255]});
+      sec = result.secStorage;
+    }
+  });
+}
+
 //Tab close listener
 //Everytime a tab is closed, this listener checks all the open tabs, and if
 //alteast one tab is youtube, nothing will happen, if no tab has youtube,
@@ -73,6 +84,7 @@ chrome.tabs.onRemoved.addListener(function() {
       });
       if (!isYoutubeOpen) {
         clearInterval(timer);
+        save();
         chrome.browserAction.setBadgeBackgroundColor({color: [127, 127, 127, 255]});
       }
     });
